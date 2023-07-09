839bc1da3a9efa0ca0a4948ed55113292d2cbe7b
Christian Bondzie <christian.zap@hotmail.fr>
2023-07-09 19:19:21 +0200

    Les cartes sont dissisable  ajout d'une snackbar et d'un action Undo

--- lib/widgets/expenses_list.dart
+++ lib/widgets/expenses_list.dart
@@ -3,15 +3,28 @@ import './expense_item.dart';
 import '../models/expense.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onDismissed});
 
   final List<Expense> expenses;
+  final Function(Expense expense) onDismissed;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+          background: Container(
+            alignment: Alignment.centerLeft,
+            decoration:
+                const BoxDecoration(color: Color.fromARGB(255, 98, 23, 18)),
+            child: const Icon(Icons.cancel),
+          ),
+          key: Key(expenses[index].title),
+          onDismissed: (direction) {
+            onDismissed(expenses[index]);
+          },
+          child: ExpenseItem(expense: expenses[index])),
     );
   }
 }
